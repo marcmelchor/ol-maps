eaa11672abc204a6ed2c08f56f7521b9de41adf2
Marc Melchor <marco.a.melchor@gmail.com>
2023-09-20 15:37:40 +0200

    refactor: Refactor component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-
-import { environment } from '../environments/environment';
-import { Map, View } from 'ol';
-import { StyleLike } from 'ol/style/Style';
-import { TopoJSON } from 'ol/format';
 import { fromLonLat } from 'ol/proj';
+import { Map, View } from 'ol';
+import { TopoJSON } from 'ol/format';
 import VectorTileLayer from 'ol/layer/VectorTile';
 import VectorTileSource from 'ol/source/VectorTile';
+
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent implements OnInit {
-  rules: StyleLike | any = [
+  rules: any = [
     {
-      filter: ['==', ['get', 'layer'], 'water'],
+      filter: [ '==', [ 'get', 'layer' ], 'water' ],
       style: {
         'fill-color': '#9db9e8',
       },
     },
     {
       else: true,
-      filter: ['all', ['==', ['get', 'layer'], 'roads'], ['get', 'railway']],
+      filter: [ 'all', [ '==', [ 'get', 'layer' ], 'roads' ], [ 'get', 'railway' ] ],
       style: {
         'stroke-color': '#7de',
         'stroke-width': 1,
-        'z-index': ['number', ['get', 'sort_key'], 0],
+        'z-index': [ 'number', [ 'get', 'sort_key' ], 0 ],
       },
     },
     {
       else: true,
-      filter: ['==', ['get', 'layer'], 'roads'],
+      filter: [ '==', [ 'get', 'layer' ], 'roads' ],
       style: {
         'stroke-color': [
           'match',
-          ['get', 'kind'],
+          [ 'get', 'kind' ],
           'major_road',
           '#776',
           'minor_road',
@@ -45,50 +44,54 @@ export class AppComponent implements OnInit {
           '#f39',
           'none',
         ],
-        'stroke-width': ['match', ['get', 'kind'], 'highway', 1.5, 1],
-        'z-index': ['number', ['get', 'sort_key'], 0],
+        'stroke-width': [ 'match', [ 'get', 'kind' ], 'highway', 1.5, 1 ],
+        'z-index': [ 'number', [ 'get', 'sort_key' ], 0 ],
       },
     },
     {
       else: true,
       filter: [
         'all',
-        ['==', ['get', 'layer'], 'buildings'],
-        ['<', ['resolution'], 10],
+        [ '==', [ 'get', 'layer' ], 'buildings' ],
+        [ '<', [ 'resolution' ], 10 ],
       ],
       style: {
-        'fill-color': '#6666',
-        'stroke-color': '#4446',
+        'fill-color': '#666',
+        'stroke-color': '#444666',
         'stroke-width': 1,
-        'z-index': ['number', ['get', 'sort_key'], 0],
+        'z-index': [ 'number', [ 'get', 'sort_key' ], 0 ],
       },
     },
   ];
+  map: Map = new Map();
 
   ngOnInit(): void {
-    new Map({
-      layers: [
+    this.osmVectorTiles();
+  }
+
+  osmVectorTiles(): void {
+    this.map.setLayers(
+      [
         new VectorTileLayer({
           source: new VectorTileSource({
-            attributions:
-              '&copy; OpenStreetMap contributors, Who’s On First, ' +
-              'Natural Earth, and osmdata.openstreetmap.de',
+            attributions: 'Map OSM',
             format: new TopoJSON({
               layerName: 'layer',
-              layers: ['water', 'roads', 'buildings'],
+              layers: [ 'water', 'roads', 'buildings' ],
             }),
             maxZoom: 16,
-            url: `https://tile.nextzen.org/tilezen/vector/v1/all/{z}/{x}/{y}.topojson?api_key=${environment.apiKey}`,
+            url: `https://tile.nextzen.org/tilezen/vector/v1/all/{z}/{x}/{y}.topojson?api_key=${ environment.netzenApiKey }`,
           }),
           style: this.rules,
         }),
       ],
-      target: 'map',
-      view: new View({
-        center: fromLonLat([-74.0064, 40.7142]),
-        maxZoom: 19,
-        zoom: 15,
-      }),
-    });
+    );
+    this.map.setTarget('map');
+    this.map.setView(new View({
+      enableRotation: true,
+      center: fromLonLat([ -74.0064, 40.7142 ]),
+      maxZoom: 19,
+      zoom: 15,
+    }));
   }
 }
